0333b3fd5b85d31ab4196e948bd5137f2ef33878
Abraham Pech <abraham.opp@gmail.com>
2023-07-20 18:35:49 -0500

    Corriendo country

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,16 @@
-async function fetching() {
-    const response = await fetch("https://restcountries.com/v3.1/all")
-    const json =  await response.json()
-    return json
-  }
-  
-  fetching(). then(
-        response => {
-            let template = ``
-            response.forEach(item => {
-                template += `
+async function fetching () {
+  const response = await fetch("https://restcountries.com/v3.1/all")
+  const json = await response.json()
+  return json
+}
+
+fetching().then(
+  response => {
+    let template = ``
+    response.forEach(item => {
+      template += `
                 <div class="item">
-                <a href="country.html?name${item.name.common}">
+                <a href="country.html?name=${item.name.common}">
                 <img src="${item.flags.png}"/>
                     <div class="item-description">
                         <h2>${item.name.official}</h2>
@@ -19,7 +19,7 @@ async function fetching() {
                 </a>
                 </div>
               `
-            })
-            result.innerHTML = template
-          }
-  )
+    })
+    result.innerHTML = template
+  }
+)
--- JS/single-country.js
+++ JS/single-country.js
@@ -1,24 +1,24 @@
-async function fetching() {
+async function fetching () {
   const url = window.location.search
   const urlParams = new URLSearchParams(url)
   const name = urlParams.get('name') || "Jordan"
-
   const response = await fetch(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
-  const json =  await response.json()
+  const json = await response.json()
   return json
 }
-  
-  fetching(). then(
-        item => {
-                let template = `
+
+fetching().then(
+  item => {
+    console.log(item)
+    let template = `
                 <div class="item">
-                <img src="${item.flags.png}"/>
+                <img src="${item[0].flags.png}"/>
                     <div class="item-description">
-                        <h2>${item.name.official}</h2>
-                        <h2>Región: ${item.region}</h2>
+                        <h2>${item[0].name.official}</h2>
+                        <h2>Región: ${item[0].region}</h2>
                     </div>
                 </div>
               `
-            result.innerHTML = template
-          }
-  )
+    result.innerHTML = template
+  }
+)
