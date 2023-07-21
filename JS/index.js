6f667300ea25eed7b99b0fbe8fbf09300746e17c
GleziDamaris <00730860@red.unid.mx>
2023-07-20 19:34:14 -0500

    crear botones mostrar y ocultar paises
    se crean dos botones donde en uno se muestresn los paises y en el otro se oculten

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,18 @@
+const countries = document.querySelector (".countries")
+const countcountries = document.querySelector ("#countries")
+const showcountries = document.querySelector ("#showcountries")
+
+countries.style.display = "none"
+showcountries.addEventListener ("click", function (){
+  countries.style.display = "none"
+  countries.style.display = "grid"
+})
+
+
+function ocultar (){
+  document.getElementById(`result`).style.display = 'none';
+}
+
 async function fetching() {
     const response = await fetch("https://restcountries.com/v3.1/all")
     const json =  await response.json()
